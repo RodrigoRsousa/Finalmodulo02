2063d21b64d28cd4c97bf5f3cd12aec0f726a627
Rodrigo Reis de Sousa <32500158+RodrigoRsousa@users.noreply.github.com>
2021-11-16 03:14:37 -0300

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,16 +1,7 @@
-// Aqui é onde farei a conexão ao meu banco de dados.
 
 const Sequelize = require("sequelize");
 
-// Aqui é feita a conexão ao meu banco local
-// const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
-//   host:  process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   dialect: 'postgres'
-// });
 
-
-// Aqui faço a conexão com o banco remoto - Heroku
 const sequelize = new Sequelize(process.env.DATABASE_URL, {     
   dialect: 'postgres',
   protocol: 'postgres',     
