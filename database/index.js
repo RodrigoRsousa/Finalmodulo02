d4b66f34d6832c613bf070a6465dfb1c1f1eaa8b
Rodrigo Reis de Sousa <32500158+RodrigoRsousa@users.noreply.github.com>
2021-11-16 01:14:10 -0300

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 
-/*const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {     
   dialect: 'postgres',
@@ -21,14 +21,7 @@ async function connected() {
   }
 }
 
-module.exports = {sequelize, connected};*/
-const Sequelize = require("sequelize");
+module.exports = {sequelize, connected};
 
-const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
-  host:  process.env.DB_HOST,
-  port: 5433,
-  dialect: 'postgres'
-});
 
-module.exports = sequelize;
 
